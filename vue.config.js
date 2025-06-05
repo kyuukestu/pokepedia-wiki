d84de550fa2448881d2022cc92b9bbe7e51f565a
kyuukestu <kyuukestu@gmail.com>
2025-06-05 15:13:47 -0500

    config

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/pokepedia-wiki/" : "/",
+	publicPath: process.env.NODE_ENV === 'production' ? '/pokepedia-wiki/' : '/',
 };
